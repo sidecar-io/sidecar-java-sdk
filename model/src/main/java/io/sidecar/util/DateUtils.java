0eca5d496fec19aa14eeadd4cc4c853670a0c139
Jason Whaley <jasonwhaley@gmail.com>
2015-07-06 14:53:50 -0400

    remove isExpiredRequestDate

--- model/src/main/java/io/sidecar/util/DateUtils.java
+++ model/src/main/java/io/sidecar/util/DateUtils.java
@@ -7,30 +7,11 @@ import org.joda.time.DateTimeZone;
 import java.text.ParseException;
 
 public class DateUtils {
-
     public static DateTime nowUtc() {
         return new DateTime(DateTimeZone.UTC);
     }
 
-    /**
-     * Test if a date is older than a certain number of minutes.
-     *
-     * @param requestDateTimeAsString - the date to check against
-     * @param minutesAgo              - the number of minutes to check against
-     * @return - true if the request date is older than the number of minutes passed in
-     */
-    @SuppressWarnings("unused")
-    public static boolean isExpiredRequestDate(String requestDateTimeAsString, int minutesAgo)
-            throws ParseException {
+    private DateUtils() {
 
-        if (StringUtils.isBlank(requestDateTimeAsString)) {
-            return true;
-        }
-
-        DateTime requestDateTime = new DateTime(requestDateTimeAsString, DateTimeZone.UTC);
-        DateTime timeOutThreshold = nowUtc().minusMinutes(minutesAgo);
-
-        // if the header date is older than this, fail
-        return requestDateTime.isBefore(timeOutThreshold);
     }
 }
