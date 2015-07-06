9e33fc0b4abd031fa9f31eda8bebc93518fb81d1
Jason Whaley <jasonwhaley@gmail.com>
2015-07-06 16:52:38 -0400

    remove unnecessary builder from ClientConfig

--- api-client/src/main/java/io/sidecar/client/ClientConfig.java
+++ api-client/src/main/java/io/sidecar/client/ClientConfig.java
@@ -1,8 +1,8 @@
 package io.sidecar.client;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Throwables.propagate;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public final class ClientConfig {
@@ -10,36 +10,22 @@ public final class ClientConfig {
     final private URL restApiBasePath;
 
     public ClientConfig() {
-        this(new Builder());
+        this(urlFromString("http://localhost:8080/rest-api"));
     }
 
-    private ClientConfig(Builder bd) {
-        restApiBasePath = getOrDefault(bd.restApiBasePath, "http://localhost:8080/rest-api");
-    }
-
-    private URL getOrDefault(URL builderUrl, String defaultUrl) {
-        try {
-            return (builderUrl != null) ? builderUrl : new URL(defaultUrl);
-        } catch (MalformedURLException e) {
-            throw propagate(e);
-        }
+    public ClientConfig(URL baseUrl) {
+        this.restApiBasePath = checkNotNull(baseUrl);
     }
 
     public URL getRestApiBasePath() {
         return restApiBasePath;
     }
 
-    public static final class Builder {
-
-        private URL restApiBasePath;
-
-        public ClientConfig build() {
-            return new ClientConfig(this);
-        }
-
-        public Builder withRestApiBasePath(URL basePath) {
-            this.restApiBasePath = basePath;
-            return this;
+    private static URL urlFromString(String s) {
+        try {
+            return new URL(s);
+        } catch (Exception e) {
+            throw propagate(e);
         }
     }
 }
