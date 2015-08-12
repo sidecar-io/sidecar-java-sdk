45cb00779d39000201a5048f3eeedba611980921
Jason Whaley <jasonwhaley@gmail.com>
2015-08-12 10:42:19 -0400

    Remove unused encrypt/decrypt methods

--- security/src/main/java/io/sidecar/security/SecurityUtils.java
+++ security/src/main/java/io/sidecar/security/SecurityUtils.java
@@ -5,9 +5,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.base.Throwables;
 import org.apache.commons.codec.binary.Hex;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import org.apache.commons.codec.binary.Base64;
@@ -51,33 +48,4 @@ public class SecurityUtils {
             throw new IllegalStateException(e);
         }
     }
-
-    /**
-     * AES encrypt
-     */
-    @SuppressWarnings("unused")
-    public static String encrypt(String plainText, String key)
-            throws Exception {
-        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        byte[] plainTextByte = plainText.getBytes();
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encryptedBytes = cipher.doFinal(plainTextByte);
-        return encodeBase64(encryptedBytes);
-    }
-
-    /**
-     * AES decrypt
-     */
-    @SuppressWarnings("unused")
-    public static String decrypt(String encryptedText, String key)
-            throws Exception {
-        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        byte[] encryptedTextBytes = Base64.decodeBase64(encryptedText.getBytes(Charset.forName("UTF-8")));
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decryptedByte = cipher.doFinal(encryptedTextBytes);
-        return new String(decryptedByte);
-    }
 }
-
